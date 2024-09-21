2111d0ea58f7b37683f11faef0fa86147c56d60a
amar-tatwa <amarjyoti.mahanta@tatwa.info>
2024-09-21 11:51:46 +0530

    login ui

---
+++ BlazorAppAuth/BlazorAppAuth.Client/Models/RequestToken.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorAppAuth.Client.Models;
+
+public class RequestToken
+{
+    public string user_name { get; set; }
+    public string password { get; set; }
+}
+
---
+++ BlazorAppAuth/BlazorAppAuth.Client/Models/TempMaster.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorAppAuth.Client.Models;
+
+public static class TempMaster
+{
+    public static List<Users> GetUsers()
+    {
+        return new List<Users> { new Users(user_name: "aa", password: "as"), new Users(user_name: "bb", password: "bs"), };
+    }
+}
+
+public record Users(string user_name, string password);
